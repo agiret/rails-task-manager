28e3caf2cb1685d9ab82252128f6b6d5dbe3a136
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2018-02-13 15:06:54 +0100

    Refacto ok

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # ...
   end
 
   def new
@@ -12,7 +14,7 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    # ...
   end
 
   def create
@@ -22,13 +24,13 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task = Task.find(params[:id])
+    # ...
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    # ...
     @task.destroy
 
     # no need for app/views/tasks/destroy.html.erb
@@ -41,5 +43,9 @@ class TasksController < ApplicationController
     # *Strong params*: You need to *whitelist* what can be updated by the user
     # Never trust user data!
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
