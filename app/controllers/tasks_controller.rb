29efd6fc89a3012a98e7fd2d66f43d7b8a378dc1
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2018-02-13 14:57:51 +0100

    Destroy ok

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,11 +27,19 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
     # *Strong params*: You need to *whitelist* what can be updated by the user
     # Never trust user data!
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
